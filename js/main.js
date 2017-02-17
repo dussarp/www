909b398057e559b206ad47f41f78d6bc836f0e4f
dussarp <pierre-yves.dussarrat@iter.org>
2017-02-17 14:32:03 +0100

    qrcode update

--- js/main.js
+++ js/main.js
@@ -29,39 +29,7 @@ var app = {
 			});
 		}
 	},
-	QrcodeEvents : function(){
-		var self = this;
-		console.log('QrCodeEvents');	
-		if(!navigator.camera){
-			app.showAlert("camera API not supported","error");
-			return;
-		}else{
-			/* BarcodeCheck */
-			cordova.plugins.barcodeScanner.scan(
-			  function (result) {
-				  alert("We got a barcode\n" +
-						"Result: " + result.text + "\n" +
-						"Format: " + result.format + "\n" +
-						"Cancelled: " + result.cancelled);
-			  },
-			  function (error) {
-				  alert("Scanning failed: " + error);
-			  },
-			  {
-				  preferFrontCamera : true, // iOS and Android
-				  showFlipCameraButton : true, // iOS and Android
-				  showTorchButton : true, // iOS and Android
-				  torchOn: true, // Android, launch with the torch switched on (if available)
-				  prompt : "Place a barcode inside the scan area", // Android
-				  resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-				  formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-				  orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
-				  disableAnimations : true, // iOS
-				  disableSuccessBeep: false // iOS
-			  }
-		   );
-		}
-	},
+	
 	route : function(){
 		var hash = window.location.hash;
 		;
@@ -118,7 +86,6 @@ var app = {
         var self = this;
 		this.detailsURL = /^#employees\/(\d{1,})/;
 		this.registerEvents();
-		this.QrcodeEvents();
         this.store = new MemoryStore(function() {
 			self.route();
         });
